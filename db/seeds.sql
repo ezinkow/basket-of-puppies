72fcc739424c3a562f5aadc8f81df3613ba17d1d
Wendi Freeman <wfreeman2112@gmail.com>
2021-01-17 21:37:55 -0600

    made changes to dog table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,9 @@
 use doggydaycare_DB;
-insert into dog (dog_name, owner_name, morning_walk, noon_walk, late_walk, medication, notes)
-values ("Spot", "Wendi Freeman", true, true, false, "none", "can be picked up by brother, Eric")
+insert into dog (dog_name, owner_id, meds, shots)
+values ("Spot", 2, "none", true);
+
+insert into activity (dog_id, morning_walk, midday_walk, late_walk, med_info, notes)
+values (1, true, false, false, "none", "likes to be petted");
+
+insert into owner (owner_name, phone, alt_pickup_name)
+values ("Wendi", "555-5555", "Eric");
