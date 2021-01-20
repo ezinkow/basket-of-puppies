9b84d7b9dc5783e38ebce86437094a3900f3dd85
Wendi Freeman <wfreeman2112@gmail.com>
2021-01-20 16:45:10 -0600

    adding connection to link to JawsDB in Heroku deployment

--- server.js
+++ server.js
@@ -6,6 +6,17 @@
 // =============================================================
 var express = require("express");
 
+if (process.env.JAWSDB_URL) {
+  var connection  = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password1',
+    database: 'doggydaycare_db'
+  });
+}
+
 // Sets up the Express App
 // =============================================================
 var app = express();
