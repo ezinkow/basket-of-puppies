72915a9cae95b3f6d061820c7dba7ee1115f6c61
Elan Zinkow <ezinkow@gmail.com>
2021-01-23 13:01:47 -0600

    fixing view dogs table formatting

--- public/assets/js/adddog.js
+++ public/assets/js/adddog.js
@@ -22,26 +22,6 @@ $(document).ready(function() {
             })
     })
 
-    // $(".addDogToOwner").on("submit", function(event) {
-    //     event.preventDefault()
-
-    //     $(".addAnotherDogToOwner").removeClass("invisible")
-    //     const newDog = {
-    //         dog_name: $("#dog_name").val().trim(),
-    //         meds: $("#meds").val().trim(),
-    //         shots: $("#shots").val().trim(),
-    //         OwnerId: $("#owner").val().trim()
-    //     }
-    //     console.log(newDog)
-
-    //     $.ajax("/api/dogs", {
-    //         type: "POST",
-    //         data: newDog
-    //     }).then(
-    //         function(res) {
-    //             // location.href = "/"
-    //         })
-    // })
 
 
 })
