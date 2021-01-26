93e84fb2d58fdbf53f5301ce4e8fa4bb9b5e0414
Elan Zinkow <ezinkow@gmail.com>
2021-01-26 11:42:44 -0600

    formatting changes, dogs show in activities view

--- routes/dog-api-routes.js
+++ routes/dog-api-routes.js
@@ -4,43 +4,68 @@ const dog = require("../models/dog");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
     //Finds all dogs but maybe doesn't do anything with the results? Maybe something calls this
-    app.get("/api/dogs/", function(req, res) {
-            db.Dog.findAll({})
-                .then(function(dbDog) {
-                    res.json(dbDog)
-                })
-        })
-        //post route adds new dog do dogs table
-    app.post("/api/dogs", function(req, res) {
-            db.Dog.create({
-                    dog_name: req.body.dog_name,
-                    shots: req.body.shots,
-                    meds: req.body.meds,
-                    notes: req.body.notes,
-                    OwnerId: req.body.OwnerId
-                })
-                .then(function(dbDog) {
-                    console.log("dbdog", dbDog)
-                    res.json(dbDog)
-                })
-        })
-        //dogs html route. Gets dogs from table, renders dogs view with this data
-    app.get("/dogs", function(req, res) {
-            db.Dog.findAll({
-                include: [db.Owner]
+    app.get("/api/dogs/", function (req, res) {
+        db.Dog.findAll({})
+            .then(function (dbDog) {
+                res.json(dbDog)
             })
-                .then(function(data) {
-                    var hbsObject = {
-                        dogs: data
-                    }
-                    console.log(hbsObject)
-                    res.render("dogs", hbsObject)
-                })
+    })
+    //post route adds new dog to dogs table
+    app.post("/api/dogs", function (req, res) {
+        db.Dog.create({
+            dog_name: req.body.dog_name,
+            shots: req.body.shots,
+            meds: req.body.meds,
+            notes: req.body.notes,
+            OwnerId: req.body.OwnerId
         })
-        //put route that updates checkin status of dog at input id. Can be used for check in or out
-    app.put("/api/dogs/:id", function(req, res) {
+            .then(function (dbDog) {
+                console.log("dbdog", dbDog)
+                res.json(dbDog)
+            })
+    })
+    //dogs html route. Gets dogs from table, renders dogs view with this data
+    app.get("/dogs", function (req, res) {
+        db.Dog.findAll({
+            include: [db.Owner]
+        })
+            .then(function (data) {
+                var hbsObject = {
+                    dogs: data
+                }
+                console.log(hbsObject)
+                res.render("dogs", hbsObject)
+            })
+    })
+
+    //quick checkin/checkout page
+    app.get("/checkinout", function (req, res) {
+        db.Dog.findAll({
+            include: [db.Owner]
+        })
+            .then(function (data) {
+                var hbsObject = {
+                    dogs: data
+                }
+                console.log("checkinout obj", hbsObject)
+                res.render("checkinout", hbsObject)
+            })
+    })
+    
+    //api to check printout
+    app.get("/api/checkinout", function (req, res) {
+        db.Dog.findAll({
+            include: [db.Owner]
+        })
+        .then(function (dbDog) {
+            console.log("dbdog", dbDog)
+            res.json(dbDog)
+            })
+    })
+    //put route that updates checkin status of dog at input id. Can be used for check in or out
+    app.put("/api/dogs/:id", function (req, res) {
         console.log("test?", req.body);
         console.log("id", req.body.id);
         console.log("check in status", req.body.check_in);
@@ -51,36 +76,36 @@ module.exports = function(app) {
             where: {
                 id: req.body.id
             }
-        }).then(function(dbPost) {
+        }).then(function (dbPost) {
             res.json(dbPost);
         });
     });
 
     //delete route deletes dog at input id
-    app.delete("/api/dogs/:id", function(req, res) {
+    app.delete("/api/dogs/:id", function (req, res) {
         db.Activity.destroy({
 
             where: {
                 id: req.params.id
             }
-        }).then(function(dbDogs) {
+        }).then(function (dbDogs) {
             res.json(dbDogs);
         });
     });
 
-        //who knows man
-        app.get("/adddogtoowner", function (req, res) {
-            db.Owner.findAll({
-                limit: 1,
-                order: [
-                    ['createdAt', 'DESC']
-                ]
-            }).then(function (data) {
-                var hbsObject = {
-                    owners: data
-                }
-                console.log("owners", data)
-                res.render("adddogtoowner", hbsObject)
-            })
+    //who knows man
+    app.get("/adddogtoowner", function (req, res) {
+        db.Owner.findAll({
+            limit: 1,
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        }).then(function (data) {
+            var hbsObject = {
+                owners: data
+            }
+            console.log("owners", data)
+            res.render("adddogtoowner", hbsObject)
         })
+    })
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,9 +4,9 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
     // at root, check if logged in. If logged in, redirect to index, if not redirect to login
-    app.get("/", function(req, res) {
+    app.get("/", function (req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
             res.redirect("/index");
@@ -14,7 +14,7 @@ module.exports = function(app) {
         res.redirect("/login");
     });
     //at login, check if logged in. If logged in, redirect to index, if not, render login page
-    app.get("/login", function(req, res) {
+    app.get("/login", function (req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
             res.redirect("/index");
@@ -22,7 +22,7 @@ module.exports = function(app) {
         res.render("../views/login.handlebars");
     });
     //at register, check if logged in. Redirect to index if logged in, otherwise render register view
-    app.get("/register", function(req, res) {
+    app.get("/register", function (req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
             res.redirect("/index");
@@ -32,27 +32,37 @@ module.exports = function(app) {
 
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
-    app.get("/index", isAuthenticated, function(req, res) {
+    app.get("/index", isAuthenticated, function (req, res) {
         res.render("../views/index.handlebars");
     })
-
+    
     //added route to get around having to login for testing purposes
-    app.get("/admin", function(req, res) {
-            res.render("../views/index.handlebars");
+    app.get("/admin", function (req, res) {
+        db.Activity.findAll({
+            include: [db.Dog]
         })
-        //at addowner, render addowner
-    app.get("/addowner", function(req, res) {
-            res.render("addowner")
+        .then(function (data) {
+            var hbsObject = {
+                activities: data
+            }
+            console.log(hbsObject)
+            res.render("index", hbsObject)
         })
-        //at daycare, render daycare
-    app.get("/daycare", function(req, res) {
-            res.render("daycare")
-        })
+    })
+    //at addowner, render addowner
+    app.get("/addowner", function (req, res) {
+        res.render("addowner")
+    })
+    
+    //at daycare, render daycare
+    app.get("/daycare", function (req, res) {
+        res.render("daycare")
+    })
 
-        //at adddog query database for all owners, then render dog view + data
-    app.get("/adddog", function(req, res) {
+    //at adddog query database for all owners, then render dog view + data
+    app.get("/adddog", function (req, res) {
         db.Owner.findAll({})
-            .then(function(data) {
+            .then(function (data) {
                 var hbsObject = {
                     owners: data
                 }
