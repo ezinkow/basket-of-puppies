03ad2a88a4bc46265b32a1bf5cc5bd5dd7349cd7
JQB <jordanqbehn@gmail.com>
2021-01-26 21:49:45 -0600

    activity put route

--- routes/activity-api-routes.js
+++ routes/activity-api-routes.js
@@ -3,11 +3,11 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function (app) {
+module.exports = function(app) {
     app.get("/activities", function(req, res) {
         db.Activity.findAll({
-            include: [db.Dog]
-        })
+                include: [db.Dog]
+            })
             .then(function(data) {
                 console.log("data:", data[0]["dataValues"]["DogId"])
                 var hbsObject = {
@@ -17,49 +17,54 @@ module.exports = function (app) {
                 // console.log(hbsObject)
                 res.render("activities", hbsObject)
             })
-        })
-
-    app.get("/api/activities", function (req, res) {
-        db.Activity.findAll({
-            include: [db.Dog],
-        })
-        .then(function(dbActivity) {
-            console.log("dbActivity", dbActivity)
-            res.json(dbActivity)
-        })
     })
-    
+
+    app.get("/api/activities", function(req, res) {
+        db.Activity.findAll({
+                include: [db.Dog],
+            })
+            .then(function(dbActivity) {
+                console.log("dbActivity", dbActivity)
+                res.json(dbActivity)
+            })
+    })
+
     app.post("/api/activities", function(req, res) {
         console.log("req body", req.body)
         db.Activity.create({
-            DogId: req.body.DogId
-        })
-        .then(function(dbActivity) {
-            console.log("dbActivity", dbActivity)
-            res.json(dbActivity)
-        })
+                DogId: req.body.DogId
+            })
+            .then(function(dbActivity) {
+                console.log("dbActivity", dbActivity)
+                res.json(dbActivity)
+            })
 
         //delete call to api/activities requires an input Dogid. Removes ALL rows with same Dogid as input
-    app.delete("/api/activities/:id", function(req, res) {
-        db.Activity.destroy({
-            where: {
-                Dogid: req.params.id
-            }
-        }).then(function(dbActivity) {
-            res.json(dbActivity);
-        });
-    });
-    })
-    
-    app.put("/api/activities/:id", function (req, res) {
-        db.Activity.update(
-            req.params.id,
-            function (result) {
-                if (result.changedRows === 0) {
-                    location.reload()
+        app.delete("/api/activities/:id", function(req, res) {
+            db.Activity.destroy({
+                where: {
+                    Dogid: req.params.id
                 }
-                res.status(200)
-            })
+            }).then(function(dbActivity) {
+                res.json(dbActivity);
+            });
+        });
+    })
+
+    app.put("/api/activities/:id", function(req, res) {
+        console.log("body", req.body)
+        console.log("params", req.params)
+        db.Activity.update({
+            morning_walk: req.body.morning_walk,
+            midday_walk: req.body.midday_walk,
+            late_walk: req.body.late_walk
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbPost) {
+            res.json(dbPost);
+        });
     })
 
 
